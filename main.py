604b0039e2191fc4a9b1856723cb5c626dd9e065
jistiak <xabid.in09@gmail.com>
2023-02-26 03:23:29 +0100

    streamlit setup v1

--- main.py
+++ main.py
@@ -5,47 +5,58 @@ import csv
 import json
 
 
-csv_file = open('Grocery_List.csv', 'r')
-json_file = open('grocery_file.json', 'w')
-
-field_names = ("item", "quantity", "unit")
-reader = csv.DictReader(csv_file, field_names)
-
-output = []
-
-for row in reader:
-    key = row["item"]
-    quantity = row["quantity"]
-    unit = row["unit"]
-    output.append({key: {"quantity": quantity, "unit": unit}})
-
-json.dump(output, json_file, indent=4)
-
-csv_file.close()
-json_file.close()
+st.title("AI Cook")
+st.markdown('')
 
 
-with open('grocery_file.json') as f:
+OPENAI_API_KEY = st.sidebar.text_input('Your OpenAI API')
+
+cuisine = st.sidebar.selectbox('Type of Cuisine', ["Random", "Chinese", "French", "Greek", "Indian",
+                                                   "Italian", "Japanese", "Korean", "Mexican", "Middle Eastern", "Spanish", "Thai"])
+
+nutrition = st.sidebar.selectbox('Nutrition Target', [
+    "Any", "Weight Loss", "Balanced", "Muscle Gain", "Cheat Day Meal"])
+
+portion = st.sidebar.select_slider('Number of Portions/People',
+                                   options=[1, 2, 3, 4, 5, "custom"])
+
+if portion == "custom":
+    portion = st.sidebar.text_input("Number of People")
+
+prep_time = st.sidebar.select_slider('Maximum Preparation Time (in minutes)', options=[
+    15, 30, 45, 60, 'Unlimited'])
+
+
+with open('sample data/grocery_file.json') as f:
     # Load the contents into a variable
     data = json.load(f)
 
 
-kitchen_items = data
-cuisine = "german"
-nutrition = "healthy"
-portion = 1
-prep_time = "30 minutes"
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = OPENAI_API_KEY
 
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+@st.cache_data
+def recipe_generator(data, cuisine, nutrition, portion, prep_time):
+    response = openai.Completion.create(
+        # model="text-ada-001",
+        model="text-davinci-003",
+        prompt=f"suggest a recipe and cooking steps based on the items in this json file {data} in {cuisine} cuisine style, with {nutrition} nutrition target in mind. The recipe should be for {portion} persons and within {prep_time} of preparation and cooking time. Don't use all the ingredients and use the best possible combination economically and realistically. Also provide the total calorie count of the meal.",
+        temperature=0.7,
+        max_tokens=512,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response
 
-response = openai.Completion.create(
-    # model="text-curie-001",
-    model="text-davinci-003",
-    prompt=f"suggest me a recipe and cooking steps based on the items in this json file {kitchen_items} in {cuisine} style, with {nutrition} nutrition value for {portion} persons within {prep_time}. Don't use all the ingredients. Use the best possible combination economically.",
-    temperature=0.7,
-    max_tokens=1000,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-)
+
+cook = st.sidebar.button('Give me something to Cook!', key='cook')
+
+if cook:
+    recipe = recipe_generator(data, cuisine, nutrition, portion, prep_time)
+    st.write(recipe['choices'][0]['text'])
+
+    with open("./sample data/response_dump.txt", "a") as file:
+        file.write(
+            f"Inputs:- Cuisine: {cuisine}, Nutrition: {nutrition}, Portion: {portion}, Prep_time: {prep_time}\nOutput:-\n{recipe}\n\n")
