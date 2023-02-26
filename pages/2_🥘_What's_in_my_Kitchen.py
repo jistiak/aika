b25f43814397f13e93c9bae1e227f15c7cca1f17
jistiak <xabid.in09@gmail.com>
2023-02-26 18:00:05 +0100

    adding ip based unique user id

--- pages/2_🥘_What's_in_my_Kitchen.py
+++ pages/2_🥘_What's_in_my_Kitchen.py
@@ -1,14 +1,40 @@
 import streamlit as st
 import json
 import pandas as pd
+import redis
+import os
+
 
 st.set_page_config(page_title="What's in my Kitchen?", page_icon="🥘")
 
 st.title("🥘 What's in my Kitchen?")
 
-with open('sample data/grocery_file.json') as f:
-    # Load the contents into a variable
-    data = json.load(f)
+r_host = os.getenv('RD_HOST')
+r_port = os.getenv('RD_PORT')
+r_pass = os.getenv('RD_PASS')
+
+
+@st.cache_data
+def redis_call(host, port, password):
+
+    r = redis.Redis(
+        host=host,
+        port=port,
+        password=password)
+
+    keys = r.keys()
+    values = r.mget(keys)
+
+    data = {}
+
+    for key, value in zip(keys, values):
+        data[f"{key.decode()}"] = f"{value.decode()}"
+
+    return data
+
+
+data = redis_call(r_host, r_port, r_pass)
+
 
 df = pd.DataFrame()
 
--- 🤖_AI_Chef.py
+++ 🤖_AI_Chef.py
@@ -5,11 +5,22 @@ import csv
 import json
 import pandas as pd
 import redis
+import hashlib
+import socket
+
+ip_address = socket.gethostbyname(socket.gethostname())
+hash_object = hashlib.sha256(ip_address.encode())
+hex_dig = hash_object.hexdigest()
+user_id = hex_dig[:8]
 
 
 st.set_page_config(
     page_title="AI Chef",
     page_icon="🤖")
+
+r_host = os.getenv('RD_HOST')
+r_port = os.getenv('RD_PORT')
+r_pass = os.getenv('RD_PASS')
 
 
 @st.cache_data
@@ -31,13 +42,11 @@ def redis_call(host, port, password):
     return data
 
 
-st.image("./sample data/dalle_cover_lynx.png", use_column_width=True)
+st.image("./assets/dalle_cover_lynx.png", use_column_width=True)
 st.title("🤖 AI Chef")
 
 
-data = redis_call(host=os.getenv('RD_HOST'),
-                  port=os.getenv('RD_PORT'),
-                  password=os.getenv('RD_PASS'))
+data = redis_call(r_host, r_port, r_pass)
 
 OPENAI_API_KEY = st.sidebar.text_input('Your OpenAI API')
 
@@ -56,6 +65,9 @@ if portion == "custom":
 prep_time = st.sidebar.select_slider('Maximum Preparation Time (in minutes)',
                                      options=[15, 30, 45, 60, 'Unlimited'])
 
+if prep_time == 'Unlimited':
+    prep_time = 180
+
 
 # openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.api_key = OPENAI_API_KEY
@@ -66,7 +78,7 @@ def recipe_generator(data, cuisine, nutrition, portion, prep_time):
     response = openai.Completion.create(
         # model="text-ada-001",
         model="text-davinci-003",
-        prompt=f"suggest a recipe and cooking steps based on the items in this json file {data} in {cuisine} cuisine style, with {nutrition} nutrition target in mind. The recipe should be for {portion} persons and within {prep_time} of preparation and cooking time. Don't use all the ingredients and use the best possible combination economically and realistically. Also provide the total calor0ie count of the meal.",
+        prompt=f"suggest a recipe and cooking steps based on the items in this json file {data} in {cuisine} cuisine style, with {nutrition} nutrition target in mind. The recipe should be for {portion} persons, only one portion per person and within {prep_time} minutes of preparation and cooking time. Don't use all the ingredients and use the best possible combination economically. give the oil, spices, salt or chillies in minimal amount. Also provide the total calorie count of the meal. the output should be a nested json format data. the user id, recipe name, ingredients, cooking step, calorie count all these should be keys, and the contents should be values.",
         temperature=0.7,
         max_tokens=512,
         top_p=1,
@@ -82,6 +94,6 @@ if cook:
     recipe = recipe_generator(data, cuisine, nutrition, portion, prep_time)
     st.write(recipe['choices'][0]['text'])
 
-    with open("./sample data/response_dump.txt", "a") as file:
+    with open("./assets/response_dump.txt", "a") as file:
         file.write(
             f"Inputs:- Cuisine: {cuisine}, Nutrition: {nutrition}, Portion: {portion}, Prep_time: {prep_time}\nOutput:-\n{recipe}\n\n")
